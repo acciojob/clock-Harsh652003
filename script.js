e654ca852e708858410e4678622ae8ab14de230f
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-07 23:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let  td=document.getElementById("timer")
+
+let cd=new Date()
+
+setInterval(
+
+    function(){
+
+        cd=new Date()
+
+        td.innerHTML=cd.toLocaleString()
+
+    }, 1000
+
+)
